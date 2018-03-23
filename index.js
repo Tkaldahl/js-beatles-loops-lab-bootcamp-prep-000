1002f89710bf78d8d41e4373e7933cbef3bc90d9
Taylor Kaldahl <tkaldahl90@gmail.com>
2018-03-23 19:04:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians, instruments){
+  const beatles = []
+  for (var i=0; i<4; i++){
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatles
+}
+
+function johnLennonFacts(facts){
+  let beatlesFacts = []
+  let i=0
+  while(facts[i]){
+    beatlesFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return beatlesFacts
+}
+
+function iLoveTheBeatles(n){
+  do {
+    console.log('I love the Beatles!')
+  }
+  while (n>0){
+    --n
+  }
+}
+
+
+
